2032d5d9ffdf80b09eea0015f763152b8911e9bb
sssssej <song050922@naver.com>
2026-09-21 15:04:28 +0900

    실습1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+int input_int;
+float input_float;
+
+printf("enter an intergar : ");
+scanf("%d", &input_int);
+
+printf("enter a float : ");
+scanf("%f", &input_float);
+
+printf("interger : %d, float : %f\n", input_int, input_float);
+
+return 0;
+}
